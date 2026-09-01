c413dd18131d3850a27e266f550241886e2fb3bd
Kevin Li <z5686986@unsw.edu.au>
2026-09-02 02:55:20 +1000

    commented out purchases for vercel to work

--- src/purchases.ts
+++ src/purchases.ts
@@ -1,15 +1,15 @@
-import { GameState, Upgrades} from "./main";
+// import { GameState, Upgrades} from "./main";
 
-function buyClickIncrease(state: GameState) {
-  if (state.lifepoints)
-  state.upgrades.clickIncrease++;
+// function buyClickIncrease(state: GameState) {
+//   if (state.lifepoints)
+//   state.upgrades.clickIncrease++;
   
-}
-
-// function getCost(state: GameState, upgrade: Upgrades): number {
-//   // checks if you have X amount of Y upgrade, and then returns the cost of it
-//   // as in it looks at how many upgrade stacks you got of the given upgrade and retuirns the cost base off that
-//   if (upgrade)
-//   state.
-//   return 0;
 // }
+
+// // function getCost(state: GameState, upgrade: Upgrades): number {
+// //   // checks if you have X amount of Y upgrade, and then returns the cost of it
+// //   // as in it looks at how many upgrade stacks you got of the given upgrade and retuirns the cost base off that
+// //   if (upgrade)
+// //   state.
+// //   return 0;
+// // }
