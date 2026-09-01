f1a8f0b51604d76d785cd9632066934b8d188fb1
Kevin Li <z5686986@unsw.edu.au>
2026-09-02 02:49:06 +1000

    added temporary frontend

--- FrontEnd/FrontEnd.ts
+++ FrontEnd/FrontEnd.ts
@@ -1,13 +1,24 @@
 import type { GameState } from '../src/main.js';
 import {clickTree} from '../src/tree.js'
+import dino_background from './assets/dinobackground.png'
+import tree_image from './assets/treelol.png'
+import bevis from './assets/bevis.png'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
+
+document.body.style.backgroundImage = `url(${dino_background})`;
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
+document.body.style.backgroundRepeat = 'no-repeat';
 
 export function render(state: GameState, onChange: () => void): void {
   app.innerHTML = `
     <h1>Incremental</h1>
+    <img src="${bevis}" alt="Bevis" class="bevis-image" />
     <p>lifepoints: <span id="count">${state.lifepoints}</span></p>
-    <button id="tree" class="tree-button">Tree</button>
+    <button id="tree" class="tree-button">
+      <img src="${tree_image}" alt="Tree" />
+    </button>
   `;
 
   document.querySelector<HTMLButtonElement>('#tree')!.addEventListener('click', () => {
